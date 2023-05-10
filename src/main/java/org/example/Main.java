fdde606494ba34de327a7864d1a458cb52a9005e
Malek-O <lslsls96996@gmail.com>
2023-05-10 23:08:43 +0300

    Add Databse connection

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import java.sql.*;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
     }
 }
